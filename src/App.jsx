6487db1812f1304041524f1538610119e71b2043
GarkarAXO <e.axel12@gmail.com>
2025-11-26 14:16:17 -0600

    Update(app):add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import HomePage from "./pages/HomePage.jsx";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
+// Importar vistas del sistema de ventas
+import ProductGrid from "./pages/sistemasDeVentas/ProductGrid.jsx";
+import VentaMostrador from "./pages/sistemasDeVentas/VentaMostrador.jsx";
+import StartDayModal from "./pages/sistemasDeVentas/StartDayModal.jsx";
+import RepartidorDashboard from "./pages/sistemasDeVentas/Repartidor/RepartidorDashboard.jsx";
+
+// Importar vistas de gestión
+import GestionDashboard from "./pages/Gestion/GestionDashboard.jsx";
+import Resumen from "./pages/Gestion/Resumen.jsx";
+import Inventario from "./pages/Gestion/Inventario.jsx";
+import Ingresos from "./pages/Gestion/Ingresos.jsx";
+import Gastos from "./pages/Gestion/Gastos.jsx";
 
 function App() {
   return (
@@ -11,6 +23,20 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/registro" element={<Register />} />
+
+      {/* Rutas: Sistema de Ventas */}
+      <Route path="/ventas/productos" element={<ProductGrid />} />
+      <Route path="/ventas/mostrador" element={<VentaMostrador />} />
+      <Route path="/ventas/inicio" element={<StartDayModal />} />
+      <Route path="/repartidor" element={<RepartidorDashboard />} />
+
+      {/* Rutas: Gestión */}
+      <Route path="/gestion" element={<GestionDashboard />}>
+        <Route index element={<Resumen />} />
+        <Route path="inventario" element={<Inventario />} />
+        <Route path="ingresos" element={<Ingresos />} />
+        <Route path="gastos" element={<Gastos />} />
+      </Route>
     </Routes>
   );
 }
